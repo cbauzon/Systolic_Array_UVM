266d63e080ede2102041d8f8009e1d0ec3df2484
CJ <cbauzon@outlook.com>
2025-10-12 13:26:18 -0700

    WIP changes made to systolic array logic

---
+++ rtl/array_cntrlr.sv
@@ -0,0 +1,18 @@
+module array_cntrlr (
+    input clk, reset_n,
+
+    // master i/f
+    output m_data,
+    output logic m_valid,
+    input m_ready,
+
+    // periph i/f
+    input p_data,
+    input p_valid,
+    output p_ready
+);
+
+
+
+
+endmodule
